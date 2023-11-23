8d22252339d57fe04f80718d658c43d9aacb4990
merhawitsegay <merhawitsegay007@gmail.com>
2023-11-22 18:04:22 -0800

    add

---
+++ activations_module.py
@@ -0,0 +1,122 @@
+from __future__ import annotations
+import random,math,torch,numpy as np,matplotlib.pyplot as plt
+import fastcore.all as fc
+from functools import partial
+
+from n_framework import *
+import torch.nn.functional as F,matplotlib as mpl
+from pathlib import Path
+from operator import attrgetter,itemgetter
+from contextlib import contextmanager
+import sys,gc,traceback
+
+from torch import tensor,nn,optim
+import torchvision.transforms.functional as TF
+from datasets import load_dataset
+
+from fastcore.test import test_close
+
+torch.set_printoptions(precision=2, linewidth=140, sci_mode=False)
+mpl.rcParams['figure.constrained_layout.use'] = True
+
+import logging
+logging.disable(logging.WARNING)
+     
+
+class Hook():
+    def __init__(self, m, f): self.hook = m.register_forward_hook(partial(f, self))
+    def remove(self): self.hook.remove()
+    def __del__(self): self.remove()
+   
+class Hooks(list):
+    def __init__(self, ms, f): super().__init__([Hook(m, f) for m in ms])
+    def __enter__(self, *args): return self
+    def __exit__ (self, *args): self.remove()
+    def __del__(self): self.remove()
+    def __delitem__(self, i):
+        self[i].remove()
+        super().__delitem__(i)
+    def remove(self):
+        for h in self: h.remove()
+
+class HooksCallback(callback):
+    def __init__(self, hookfunc, mod_filter=fc.noop, on_train=True, on_valid=False, mods=None):
+        fc.store_attr()
+        super().__init__()
+    
+    def before_fit(self, learn):
+        if self.mods: mods=self.mods
+        else: mods = fc.filter_ex(learn.model.modules(), self.mod_filter)
+        self.hooks = Hooks(mods, partial(self._hookfunc, learn))
+
+    def _hookfunc(self, learn, *args, **kwargs):
+        if (self.on_train and learn.training) or (self.on_valid and not learn.training): self.hookfunc(*args, **kwargs)
+
+    def after_fit(self, learn): self.hooks.remove()
+    def __iter__(self): return iter(self.hooks)
+    def __len__(self): return len(self.hooks)
+     
+
+def append_stats(hook, mod, inp, outp):
+    if not hasattr(hook,'stats'): hook.stats = ([],[],[])
+    acts = to_cpu(outp)
+    hook.stats[0].append(acts.mean())
+    hook.stats[1].append(acts.std())
+    hook.stats[2].append(acts.abs().histc(40,0,10))
+     
+def get_hist(h): return torch.stack(h.stats[2]).t().float().log1p()
+ 
+def get_min(h):
+    h1 = torch.stack(h.stats[2]).t().float()
+    return h1[0]/h1.sum(0)
+  
+class ActivationStats(HooksCallback):
+    def __init__(self, mod_filter=fc.noop): super().__init__(append_stats, mod_filter)
+
+    def color_dim(self, figsize=(11,5)):
+        fig,axes = get_grid(len(self), figsize=figsize)
+        for ax,h in zip(axes.flat, self):
+            show_image(get_hist(h), ax, origin='lower')
+
+    def dead_chart(self, figsize=(11,5)):
+        fig,axes = get_grid(len(self), figsize=figsize)
+        for ax,h in zip(axes.flatten(), self):
+            ax.plot(get_min(h))
+            ax.set_ylim(0,1)
+
+    def plot_stats(self, figsize=(10,4)):
+        fig,axs = plt.subplots(1,2, figsize=figsize)
+        for h in self:
+            for i in 0,1: axs[i].plot(h.stats[i])
+        axs[0].set_title('Means')
+        axs[1].set_title('Stdevs')
+        plt.legend(fc.L.range(self))
+     
+#|export
+def clean_mem():
+    clean_tb()
+    clean_ipython_hist()
+    gc.collect()
+    torch.cuda.empty_cache()
+     
+def clean_ipython_hist():
+    # Code in this function mainly copied from IPython source
+    if not 'get_ipython' in globals(): return
+    ip = get_ipython()
+    user_ns = ip.user_ns
+    ip.displayhook.flush()
+    pc = ip.displayhook.prompt_count + 1
+    for n in range(1, pc): user_ns.pop('_i'+repr(n),None)
+    user_ns.update(dict(_i='',_ii='',_iii=''))
+    hm = ip.history_manager
+    hm.input_hist_parsed[:] = [''] * pc
+    hm.input_hist_raw[:] = [''] * pc
+    hm._i = hm._ii = hm._iii = hm._i00 =  ''
+
+def clean_tb():
+   
+    if hasattr(sys, 'last_traceback'):
+        traceback.clear_frames(sys.last_traceback)
+        delattr(sys, 'last_traceback')
+    if hasattr(sys, 'last_type'): delattr(sys, 'last_type')
+    if hasattr(sys, 'last_value'): delattr(sys, 'last_value')
