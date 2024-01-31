902f52fae58bef9edee4f36cc9eecd1a142e9735
merhawitsegay <merhawitsegay007@gmail.com>
2024-01-30 18:11:29 -0800

    add

--- activations_module.py
+++ activations_module.py
@@ -250,3 +250,25 @@ class ResBlock(nn.Module):
         self.act    = act() 
         
     def forward(self, x): return self.act(self.convs(x) + self.idconv(self.pool(x)))
+
+def _rand_erase1(x, pct, xm, xs, mn, mx):
+    szx = int(pct*x.shape[-2])
+    szy = int(pct*x.shape[-1])
+    stx = int(random.random()*(1-pct)*x.shape[-2])
+    sty = int(random.random()*(1-pct)*x.shape[-1])
+    init.normal_(x[:,:,stx:stx+szx,sty:sty+szy], mean=xm, std=xs)
+    x.clamp_(mn, mx)
+
+def rand_erase(x, pct=0.2, max_num = 4):
+    xm,xs,mn,mx = x.mean(),x.std(),x.min(),x.max()
+    num = random.randint(0, max_num)
+    for i in range(num): _rand_erase1(x, pct, xm, xs, mn, mx)
+    return x
+
+class RandErase(nn.Module):
+    def __init__(self, pct=0.2, max_num=4):
+        super().__init__()
+        self.pct,self.max_num = pct,max_num
+    def forward(self, x): return rand_erase(x, self.pct, self.max_num)
+     
+     
